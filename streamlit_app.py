69aa7a8e7bda381e0af0c304c2148a52e134e686
sigirinaveen <149869382+sigirinaveen@users.noreply.github.com>
2023-11-05 10:13:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
+streamlit.title('My parents New Healthy Diner')
 
-streamlit.title('Hi Naveen Sigiri')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
