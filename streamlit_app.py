2df5da63e7134f6524cc00ff9f3b42f9cfd457b4
sigirinaveen <149869382+sigirinaveen@users.noreply.github.com>
2023-11-06 11:53:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ streamlit.header('Fruityvice Fruit Advice!')
 # write your own comment -what does the next line do? 
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-s#treamlit.dataframe(fruityvice_normalized)
+#treamlit.dataframe(fruityvice_normalized)
